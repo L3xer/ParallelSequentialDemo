b56adac4f07e03b96b02cedd5e9e0adfa7b40ce2
sanya andreichuk <sanya2371@gmail.com>
2018-06-11 07:53:18 +0300

    Bind commands.

--- ParallelSequentialDemo/ParallelSequentialDemo/ViewModels/MainPageViewModel.cs
+++ ParallelSequentialDemo/ParallelSequentialDemo/ViewModels/MainPageViewModel.cs
@@ -1,13 +1,34 @@
-﻿using Prism.Navigation;
+﻿using System;
+using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services;
 
 namespace ParallelSequentialDemo.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService) 
-            : base (navigationService)
-        {
+        public DelegateCommand ParallelCommand { get; set; }
+        public DelegateCommand SequentialCommand { get; set; }
 
+        private IPageDialogService _dialogService;
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+            : base(navigationService)
+        {
+            _dialogService = dialogService;
+
+            ParallelCommand = new DelegateCommand(ParallelExecute);
+            SequentialCommand = new DelegateCommand(SequentialExecute);
+        }
+
+        private void SequentialExecute()
+        {
+            _dialogService.DisplayAlertAsync("Test", "Sequential clicked", "OK");
+        }
+
+        private void ParallelExecute()
+        {
+            _dialogService.DisplayAlertAsync("Test", "Paralell clicked", "OK");
         }
     }
 }
